524a201c44923aba1e97162f5d4a3bc91527dba5
Andre Santos (AB) <afsantos@stone.com.br>
2024-01-22 23:28:54 -0300

    feat: entity Client

---
+++ src/main/java/com/dexdev/loansimulation/modules/client/entities/Client.java
@@ -0,0 +1,71 @@
+package com.dexdev.loansimulation.modules.client.entities;
+
+import java.time.LocalDate;
+
+public class Client {
+    private int id;
+    private String name;
+    private LocalDate birthDate;
+
+    private String ssn;
+    private String ssnType;
+
+    public Client(int id, String name, LocalDate birthDate, String ssn, String ssnType) {
+        this.id = id;
+        this.name = name;
+        this.birthDate = birthDate;
+        this.ssn = ssn;
+        this.ssnType = ssnType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getSsnType() {
+        return ssnType;
+    }
+
+    public void setSsnType(String ssnType) {
+        this.ssnType = ssnType;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", birthDate=" + birthDate +
+                ", ssn='" + ssn + '\'' +
+                ", ssnType='" + ssnType + '\'' +
+                '}';
+    }
+}
