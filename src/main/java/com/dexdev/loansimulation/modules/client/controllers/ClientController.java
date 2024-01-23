79918c55ee4fdb1e6230041878613392d78f4e5b
André Santos <andre@dexdev.com.br>
2024-01-23 00:00:41 -0300

    refactor: client controller
    - use GetMapping decorator

--- src/main/java/com/dexdev/loansimulation/modules/client/controllers/ClientController.java
+++ src/main/java/com/dexdev/loansimulation/modules/client/controllers/ClientController.java
@@ -1,8 +1,8 @@
 package com.dexdev.loansimulation.modules.client.controllers;
 
 import com.dexdev.loansimulation.modules.client.entities.Client;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
@@ -12,7 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/clients")
 public class ClientController {
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @GetMapping
     public List<Client> all() {
         return Arrays.asList(
                 new Client(1, "Andre Santos", LocalDate.parse("1983-08-25"), "1234567890", "RG"),
