38ddbd026d86b5e6b7167a0864a9af04d8be7294
André Santos <andre@dexdev.com.br>
2024-01-28 12:18:09 -0300

    refactor: entities Product and InstallmentTax

---
+++ src/main/java/com/dexdev/loansimulation/modules/product/entities/InstallmentTax.java
@@ -0,0 +1,36 @@
+package com.dexdev.loansimulation.modules.product.entities;
+
+
+public class InstallmentTax {
+    private int number;
+    private float value;
+
+    public InstallmentTax(int instalmentNumber, float value) {
+        this.number = instalmentNumber;
+        this.value = value;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Tax{" +
+                "number=" + number +
+                ", value=" + value +
+                '}';
+    }
+}
--- src/main/java/com/dexdev/loansimulation/modules/product/entities/Product.java
+++ src/main/java/com/dexdev/loansimulation/modules/product/entities/Product.java
@@ -1,12 +1,26 @@
 package com.dexdev.loansimulation.modules.product.entities;
 
+import java.util.List;
+import java.util.Set;
+
+
 public class Product {
     private int id;
     private String name;
 
+    private List<InstallmentTax> taxes;
+
     public Product(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public List<InstallmentTax> getTaxes() {
+        return taxes;
+    }
+
+    public void setTaxes(List<InstallmentTax> taxes) {
+        this.taxes = taxes;
     }
 
     public int getId() {
