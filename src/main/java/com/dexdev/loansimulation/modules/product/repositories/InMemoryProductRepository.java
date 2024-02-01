38c98491e22836b36d5deb545de08a4d5eec7a8f
André Santos <andre@dexdev.com.br>
2024-01-31 21:07:13 -0300

    refactor: complete installments tax

--- src/main/java/com/dexdev/loansimulation/modules/product/repositories/InMemoryProductRepository.java
+++ src/main/java/com/dexdev/loansimulation/modules/product/repositories/InMemoryProductRepository.java
@@ -14,10 +14,18 @@ public class InMemoryProductRepository implements ProductRepository {
         Product product = new Product(1, "EasyMoney");
         List<InstallmentTax> taxes = Arrays
                 .asList(
-                new InstallmentTax(1, 10f),
-                new InstallmentTax(2, 10f),
-                new InstallmentTax(3, 5f),
-                new InstallmentTax(4, 2.5f)
+                new InstallmentTax(1, 25),
+                new InstallmentTax(2, 37.1f),
+                new InstallmentTax(3, 37.1f),
+                new InstallmentTax(4, 39),
+                new InstallmentTax(5, 39),
+                new InstallmentTax(6, 39),
+                new InstallmentTax(7, 39),
+                new InstallmentTax(8, 39),
+                new InstallmentTax(9, 39),
+                new InstallmentTax(10, 39),
+                new InstallmentTax(11, 39),
+                new InstallmentTax(12, 39)
         );
 
         product.setTaxes(taxes);
@@ -26,6 +34,13 @@ public class InMemoryProductRepository implements ProductRepository {
 
     public List<Product> all() {
         return products;
+    }
+
+    public Product getById(int productId) {
+        return products.stream()
+                .filter(p -> p.getId() == productId)
+                .findFirst()
+                .orElse(null);
     }
 
     public InstallmentTax getInstallmentByIdAndProduct(Integer productId, Integer installment) {
